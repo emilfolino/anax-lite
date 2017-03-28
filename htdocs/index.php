ac7deabfd87e26a95312d19c30d2d7185d03e026
Emil Folino <emil.folino@bth.se>
2017-03-28 12:58:21 +0200

    Added config and request via composer

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,21 @@
-<?php echo "I wanna be a frontcontroller!";
+<?php
+
+/**
+ * Bootstrap the framework.
+ */
+// Where are all the files? Booth are needed by Anax.
+define("ANAX_INSTALL_PATH", realpath(__DIR__ . "/.."));
+define("ANAX_APP_PATH", ANAX_INSTALL_PATH);
+
+// Include essentials
+require ANAX_INSTALL_PATH . "/config/error_reporting.php";
+
+// Get the autoloader by using composers version.
+require ANAX_INSTALL_PATH . "/vendor/autoload.php";
+
+// Create and use an object of the request class.
+$request = new \Anax\Request\Request();
+$request->init();
+var_dump($request);
+
+echo "Done";
